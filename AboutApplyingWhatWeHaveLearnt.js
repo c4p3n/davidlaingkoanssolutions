e0046a445534cd3cafc641e8649065524ab6ffa0
Capen Rhew <capenr@gmail.com>
2016-01-19 18:12:31 -0500

    My solutions to the koans
    I did not complete the last challenge due to not understanding how to make the flatten function work.

---
+++ AboutApplyingWhatWeHaveLearnt.js
@@ -0,0 +1,144 @@
+var _; //globals
+
+describe("About Applying What We Have Learnt", function() {
+
+  var products;
+
+  beforeEach(function () {
+    products = [
+       { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+       { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+       { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+       { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+       { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+    ];
+  });
+
+  /*********************************************************************************/
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
+    var i,j,hasMushrooms, productsICanEat = [];
+
+    for (i = 0; i < products.length; i+=1) {
+        if (products[i].containsNuts === false) {
+            hasMushrooms = false;
+            for (j = 0; j < products[i].ingredients.length; j+=1) {
+               if (products[i].ingredients[j] === "mushrooms") {
+                  hasMushrooms = true;
+               }
+            }
+            if (!hasMushrooms) productsICanEat.push(products[i]);
+        }
+    }
+
+    expect(productsICanEat.length).toBe(1);
+  });
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+
+      var productsICanEat = [];
+
+      /* solve using filter() & all() / any() */
+     var hasMushrooms = function(x) { return x === "mushrooms";  };
+      
+     // return an array of all products that do not contain nuts 
+     productsICanEat = _(products).filter(function (x) { return x.containsNuts === false });
+     
+     // get rid of remaining products that also have mushrooms as an ingredient
+     for (i = 0; i < products.length; i+=1) {
+		  if(_(products[i].ingredients).any(hasMushrooms))
+            productsICanEat.pop(products[i]);
+     }
+     
+	   
+      expect(productsICanEat.length).toBe(1);
+  });
+
+  /*********************************************************************************/
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+
+    var sum = 0;
+    for(var i=1; i<1000; i+=1) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+
+    expect(sum).toBe(233168);
+  });
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+
+    var sum = _(1000).chain().range().reduce( function(memo, x)
+        { 
+            if (x % 3 === 0 || x % 5 === 0)
+                    memo += x;
+                return memo;
+        }, 0).value();   
+      
+       /* try chaining range() and reduce() */
+
+    expect(233168).toBe(sum);
+  });
+
+  /*********************************************************************************/
+   it("should count the ingredient occurrence (imperative)", function () {
+    var ingredientCount = { "{ingredient name}": 0 };
+
+    for (i = 0; i < products.length; i+=1) {
+        for (j = 0; j < products[i].ingredients.length; j+=1) {
+            ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+            // Capen's notes: making this an || 0 turns the occurrence of an ingredient into a 1 instead of leaving it as a word, so it can be added to a sum.
+        }
+    }
+
+    expect(ingredientCount['mushrooms']).toBe(2);
+  });
+
+  it("should count the ingredient occurrence (functional)", function () {
+    var ingredientCount = { "{ingredient name}": 0 };
+
+    /* chain() together map(), flatten() and reduce() */
+      
+    for (i = 0; i < products.length; i += 1) {
+        for (j = 0; j < products[i].ingredients.length; j+=1) {
+            ingredientCount[products[i].ingredients[j]] = _(products).chain().flatten().map(function (x) { 
+            if (x === products[i].ingredients[j]){
+                x = 1;
+            }
+            else x = 0;
+            return x;}).reduce(function (sum, x) { return sum + x }).value();
+        }
+        
+    }
+
+    expect(ingredientCount['mushrooms']).toBe(_(products).flatten());
+  });
+
+  /*********************************************************************************/
+  /* UNCOMMENT FOR EXTRA CREDIT */
+  /*
+  it("should find the largest prime factor of a composite number", function () {
+
+  });
+
+  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+
+  });
+
+  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+
+
+  });
+
+  it("should find the difference between the sum of the squares and the square of the sums", function () {
+
+  });
+
+  it("should find the 10001st prime", function () {
+
+  });
+  */
+});
